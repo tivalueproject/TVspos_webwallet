0b543d27191c1ebe30364b691790e827c2ec3999
ocean <3408598@qq.com>
2019-04-26 18:03:40 +0800

    modify version

--- src/components/home/modals/About.vue
+++ src/components/home/modals/About.vue
@@ -15,14 +15,14 @@
       <div class="image-illustration">
         <img src="../../../assets/imgs/icons/wallet/ic_illustration_wallet.svg">
       </div>
-      <div class="about-vsyswallet">About V Wallet
+      <div class="about-vsyswallet">About TV Wallet
       </div>
       <div class="version">Version {{ vnumber }}
       </div>
       <div class="about_text">
-        V Wallet is a reference implementation of cross-platform browser wallet for the V Systems blockchain network. It features basic hot wallet mode and cold wallet interaction/monitoring mode. It implements V Systems wallet format specification 1.0 and V Systems wallet QR code API 2.0.
+        TV Wallet is a reference implementation of cross-platform browser wallet for the blockchain network. It features basic hot wallet mode and cold wallet interaction/monitoring mode.
       </div>
-      <div class="right-reserved">@2018-2019 V SYSTEMS LIMITED All rights reserved
+      <div class="right-reserved">@2019 TV LIMITED All rights reserved
       </div>
     </div>
   </b-modal>
