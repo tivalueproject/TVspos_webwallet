c86ee0f2f58fee5ed7725b8e3a6dd815d998c31e
ocean <3408598@qq.com>
2019-04-27 13:08:40 +0800

    modify networkid

--- src/constants.js
+++ src/constants.js
@@ -11,9 +11,9 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
 };
 export const WALLET_VERSION = '0.1.1'
 export const INITIAL_SESSION_TIMEOUT = 5
-export const NETWORK_BYTE = 'M'.charCodeAt(0);
+export const NETWORK_BYTE = 'A'.charCodeAt(0);
 export const INITIAL_NONCE = 0;
-export const ADDRESS_VERSION = 1;
+export const ADDRESS_VERSION = 5;
 export const PAYMENT_TX = 2;
 export const LEASE_TX = 3;
 export const CANCEL_LEASE_TX = 4;
