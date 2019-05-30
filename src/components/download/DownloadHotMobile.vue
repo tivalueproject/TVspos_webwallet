7b2fcc2072f706c585e6b8333ee7564403e2d62b
ocean <3408598@qq.com>
2019-05-30 13:22:08 +0800

    add download

--- src/components/download/DownloadHotMobile.vue
+++ src/components/download/DownloadHotMobile.vue
@@ -32,7 +32,7 @@ export default {
         return {
             title: 'TV HOT',
             twoPlanarCode: require('../../assets/imgs/download/ico_welcome.png'),
-            iphoneAppResource: 'itms-services://?action=download-manifest&url=',
+            iphoneAppResource: '',
             androidAppResource: 'https://link-e-pro.oss-cn-beijing.aliyuncs.com/wallet/hot.wallet_release.apk'
         }
     },
