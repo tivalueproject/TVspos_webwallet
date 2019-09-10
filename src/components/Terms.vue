7d31da526c2cb030e4b189075614a6e5be2bfca4
ocean <3408598@qq.com>
2019-09-10 11:04:57 +0800

    terms

--- src/components/Terms.vue
+++ src/components/Terms.vue
@@ -19,8 +19,8 @@
       </div>
       <div class="txt"
            align="justify">
-        <p>  1.1. This agreement is between you ("You" or "Your" in this agreement) and V SYSTEMS Limited ("V SYSTEMS" in this agreement). V Wallet is a cryptocurrency wallet application base on V SYSTEMS blockchain. You acknowledged that any use of V Wallet is subject to these Terms of Service.</p>
-        <p>  1.2. It's your own responsibility to read the Terms of Service carefully before you start to use V Wallet.</p>
+        <p>  1.1. This agreement is between you ("You" or "Your" in this agreement) and TV SYSTEMS Limited ("TV SYSTEMS" in this agreement). TV Wallet is a cryptocurrency wallet application base on TV SYSTEMS blockchain. You acknowledged that any use of TV Wallet is subject to these Terms of Service.</p>
+        <p>  1.2. It's your own responsibility to read the Terms of Service carefully before you start to use TV Wallet.</p>
         <br>
       </div>
       <div class="smalltitle">
@@ -29,13 +29,13 @@
       </div>
       <div class="txt"
            align="justify">
-        <p>V Wallet is an innovative cryptocurrency wallet service providing for the V SYSTEMS blockchain, which consists of:</p>
-        <p>2.1. V Wallet Web Application</p>
-        <p>V SYSTEMS has created means for users to access and transfer ownership of V coins under the name of V Wallet. By entering into this agreement You will receive a license to use any of the V Wallet Web Application. It is strongly recommended You only use the V Wallet Web Application after gaining basic knowledge of cryptocurrencies.</p>
+        <p>TV Wallet is an innovative cryptocurrency wallet service providing for the TV SYSTEMS blockchain, which consists of:</p>
+        <p>2.1. TV Wallet Web Application</p>
+        <p>TV SYSTEMS has created means for users to access and transfer ownership of TV coins under the name of TV Wallet. By entering into this agreement You will receive a license to use any of the TV Wallet Web Application. It is strongly recommended You only use the TV Wallet Web Application after gaining basic knowledge of cryptocurrencies.</p>
         <p>2.1.1. You Own Your Private Keys</p>
-        <p>V SYSTEMS does not store user’s private keys, phrases or passwords ("Private Information") on any servers. It is very important that You backup Private Information. If You lose Your Private Information then it will not be possible for V SYSTEMS to recover it for You and You may lose access to Your V coins. If You do not understand the above paragraph then we strongly recommend that You learn more about cryptocurrencies before using the V Wallet Web Application.</p>
+        <p>TV SYSTEMS does not store user’s private keys, phrases or passwords ("Private Information") on any servers. It is very important that You backup Private Information. If You lose Your Private Information then it will not be possible for TV SYSTEMS to recover it for You and You may lose access to Your TV coins. If You do not understand the above paragraph then we strongly recommend that You learn more about cryptocurrencies before using the TV Wallet Web Application.</p>
         <p>2.1.2. Transactions</p>
-        <p>The only authentic record of cryptocurrency transactions is the applicable cryptocurrency blockchain. The V Wallet Web Application provides functionality that allows You to receive, send, lease and cancel lease cryptocurrency transaction instructions. V SYSTEMS does not guarantee that all transactions You perform using the V Wallet Web Application shall be stored in any cryptocurrency blockchain. You must ensure that Your transactions conform to the applicable rules of the cryptocurrency software. There may be transaction fees associated with Your cryptocurrency transactions that are required by the cryptocurrency system You engaged with. V SYSTEMS should not be responsible for any losses You incur due to transaction fees or losses that occur due to incorrectly set transaction fees (i.e. too low or high). V SYSTEMS does not have access to your transactions (or anyone else's).</p>
+        <p>The only authentic record of cryptocurrency transactions is the applicable cryptocurrency blockchain. The TV Wallet Web Application provides functionality that allows You to receive, send, lease and cancel lease cryptocurrency transaction instructions. TV SYSTEMS does not guarantee that all transactions You perform using the TV Wallet Web Application shall be stored in any cryptocurrency blockchain. You must ensure that Your transactions conform to the applicable rules of the cryptocurrency software. There may be transaction fees associated with Your cryptocurrency transactions that are required by the cryptocurrency system You engaged with. TV SYSTEMS should not be responsible for any losses You incur due to transaction fees or losses that occur due to incorrectly set transaction fees (i.e. too low or high). TV SYSTEMS does not have access to your transactions (or anyone else's).</p>
         <br>
       </div>
       <div class="smalltitle">
@@ -46,14 +46,14 @@
            align="justify">
         <p>You shall:<br>
           be of the age of majority in Your local jurisdiction; and,<br>
-          be responsible for Your own V coins; and,<br>
+          be responsible for Your own TV coins; and,<br>
           backup/write down Your seed phrase for wallet recovery and keep it safe and private; and,<br>
-          carefully guard Your seed phrase in case of usage V Wallet Web Application; and,<br>
+          carefully guard Your seed phrase in case of usage TV Wallet Web Application; and,<br>
           ensure that Your device(s) are fully updated and do not contain viruses, malware or otherwise malicious software.<br>
           You shall not:<br>
-          deposit V coins into addresses for coins that are not supported by the V Wallet Web Application; or,<br>
-          use V Wallet in a manner likely to interfere with other V SYSTEMS blockchain users or nodes (e.g. excessive API calls or network spam); or,<br>
-          use V Wallet in a manner contrary to Your local laws.
+          deposit TV coins into addresses for coins that are not supported by the TV Wallet Web Application; or,<br>
+          use TV Wallet in a manner likely to interfere with other TV SYSTEMS blockchain users or nodes (e.g. excessive API calls or network spam); or,<br>
+          use TV Wallet in a manner contrary to Your local laws.
         </p>
         <br>
       </div>
@@ -63,7 +63,7 @@
       </div>
       <div class="txt"
            align="justify">
-        <p>This agreement licenses V Wallet services to You on a personal, non-exclusive, royalty-free, non-transferable, worldwide basis, for the purpose of managing cryptocurrencies. You are not permitted to re-distribute any of V Wallet services, modify any code or use any V Wallet content, including images and text, as part of any other software or project of any kind.</p>
+        <p>This agreement licenses TV Wallet services to You on a personal, non-exclusive, royalty-free, non-transferable, worldwide basis, for the purpose of managing cryptocurrencies. You are not permitted to re-distribute any of TV Wallet services, modify any code or use any TV Wallet content, including images and text, as part of any other software or project of any kind.</p>
         <br>
       </div>
       <div class="smalltitle">
@@ -72,13 +72,13 @@
       </div>
       <div class="txt"
            align="justify">
-        <p>Using cryptocurrency software necessarily entails risks. V SYSTEMS specifically disclaims and shall have no liability to You for loss incurred by any risk including but not limited to the following:<br>
+        <p>Using cryptocurrency software necessarily entails risks. TV SYSTEMS specifically disclaims and shall have no liability to You for loss incurred by any risk including but not limited to the following:<br>
           operating system failures (mobile or desktop); and,<br>
-          interactions between Your hardware, software, and the V Wallet; and,<br>
+          interactions between Your hardware, software, and the TV Wallet; and,<br>
           cloud backup software (e.g. certain distributions) may upload your private information to third party services; and,<br>
-          malware, viruses or other malicious software on Your device that is able to take control of or interfere with V Wallet; and,<br>
-          communication delays between Your V Wallet and a node or relay service for a cryptocurrency (and vice versa); and,<br>
-          failure to achieve a certain market value/price for the cryptocurrency, whether through a third-party service or any other kind of transaction; and, theft of V coins.
+          malware, viruses or other malicious software on Your device that is able to take control of or interfere with TV Wallet; and,<br>
+          communication delays between Your TV Wallet and a node or relay service for a cryptocurrency (and vice versa); and,<br>
+          failure to achieve a certain market value/price for the cryptocurrency, whether through a third-party service or any other kind of transaction; and, theft of TV coins.
         </p>
         <br>
       </div>
@@ -88,7 +88,7 @@
       </div>
       <div class="txt"
            align="justify">
-        <p>V Wallet is provided to You at no cost, "as-is" and without any warranty whatsoever, to the maximum extent permissible by law. Without limiting the generality of the foregoing, V SYSTEMS does not warrant that V Wallet is fit for Your purpose, even if You have previously provided notice of Your intended purpose, and does not warrant that V Wallet will operate in a bug-free manner.</p>
+        <p>TV Wallet is provided to You at no cost, "as-is" and without any warranty whatsoever, to the maximum extent permissible by law. Without limiting the generality of the foregoing, TV SYSTEMS does not warrant that TV Wallet is fit for Your purpose, even if You have previously provided notice of Your intended purpose, and does not warrant that TV Wallet will operate in a bug-free manner.</p>
         <br>
       </div>
       <div class="smalltitle">
@@ -97,9 +97,9 @@
       </div>
       <div class="txt"
            align="justify">
-        <p>V SYSTEMS shall have no liability to loss incurred by defects or malfunction of V Wallet;<br>
-          V SYSTEMS shall have no liability to loss incurred by any loss of service, or hacking, virus, malware, hijacking attack of the V Wallet service;<br>
-          V SYSTEMS  shall have no liability to loss incurred by  the use of V Wallet for investment purpose and related activities.
+        <p>TV SYSTEMS shall have no liability to loss incurred by defects or malfunction of TV Wallet;<br>
+          TV SYSTEMS shall have no liability to loss incurred by any loss of service, or hacking, virus, malware, hijacking attack of the TV Wallet service;<br>
+          TV SYSTEMS  shall have no liability to loss incurred by  the use of TV Wallet for investment purpose and related activities.
         </p>
         <br>
       </div>
@@ -109,7 +109,7 @@
       </div>
       <div class="txt"
            align="justify">
-        <p>You agree to indemnify V SYSTEMS against any claims, damages, losses or legal fees incurred due to Your use (or misuse) of V Wallet.</p>
+        <p>You agree to indemnify TV SYSTEMS against any claims, damages, losses or legal fees incurred due to Your use (or misuse) of TV Wallet.</p>
         <br>
       </div>
       <div class="smalltitle">
@@ -118,7 +118,7 @@
       </div>
       <div class="txt"
            align="justify">
-        <p>No action or inaction on the part of V SYSTEMS shall be considered a waiver of any right or obligation by V SYSTEMS.</p>
+        <p>No action or inaction on the part of TV SYSTEMS shall be considered a waiver of any right or obligation by TV SYSTEMS.</p>
         <br>
       </div>
       <div class="smalltitle">
@@ -127,7 +127,7 @@
       </div>
       <div class="txt"
            align="justify">
-        <p>You may not assign this agreement. V SYSTEMS may assign this agreement to 30 days notice.</p>
+        <p>You may not assign this agreement. TV SYSTEMS may assign this agreement to 30 days notice.</p>
         <br>
       </div>
       <div class="smalltitle">
